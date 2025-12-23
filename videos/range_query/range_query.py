683fce46878bbfdc20b9b27fc27fa2573e8557aa
taylor.wong2 <taylor.wong2@cs.ucalgary.ca>
2025-12-23 14:57:22 -0700

    added test video

---
+++ videos/range_query/range_query.py
@@ -0,0 +1,13 @@
+from manim import *
+
+class range_query(Scene):
+
+    def init(self) -> None:
+        ...
+
+    def construct(self) -> None:
+        # Initialize variables
+        self.init()
+
+        # Animate Scene
+        ...
