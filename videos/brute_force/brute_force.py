498bd448da4c63c621c9ecdd10e5ce1f6ddfe7cc
taylor.wong2 <taylor.wong2@cs.ucalgary.ca>
2026-01-19 16:54:49 -0700

    updated brute force video

--- videos/brute_force/brute_force.py
+++ videos/brute_force/brute_force.py
@@ -2,12 +2,19 @@ from manim import *
 
 class BruteForce(Scene):
 
-    def init(self) -> None:
-        # Initialization code goes here
-        ...
-
     def construct(self) -> None:
-        self.init()
+        title = Text('Brute Force', font_size=56)
 
-        # Your animation code goes here
-        ...
+        # Show Title
+        self.wait(4)
+        self.play(Create(title))
+        self.wait(1)
+
+        # Transition into definition
+        self.play(title.animate.move_to((0, 3, 0)))
+        definition = Text(
+            'Try all possible "solutions" until a "valid" solution is found',
+            font_size=24,
+        ).next_to(title, DOWN)
+        self.play(Create(definition))
+        self.wait(1)
