a48727fbb12881c6c0c342f8cb16625b37a2776c
taylor.wong2 <taylor.wong2@cs.ucalgary.ca>
2026-01-19 10:47:02 -0700

    started brute force video

---
+++ videos/brute_force/brute_force.py
@@ -0,0 +1,13 @@
+from manim import *
+
+class BruteForce(Scene):
+
+    def init(self) -> None:
+        # Initialization code goes here
+        ...
+
+    def construct(self) -> None:
+        self.init()
+
+        # Your animation code goes here
+        ...
